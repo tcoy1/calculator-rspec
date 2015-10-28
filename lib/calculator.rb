9490ec9e90a7ea90093af5b8bb6438b4cb57e4ef
Tyler Coy <tcoy1@student.cscc.edu>
2015-10-27 21:09:30 -0400

    add is done - need sub, mult, div

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,7 +1,12 @@
 class Calculator
 	
-	def add(n1, n2)
-		return 1
+	def add(*n1)
+		sum = n1.inject(:+)
+		return sum
 	end
 	
 end
+
+
+# * in *n1 is splat operator -> variable number of variables!
+# inject loops over object with right of : and returns to left.
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -9,6 +9,12 @@ describe Calculator do
 			actual = calc.add(2,3)
 			expect(actual).to be_a Integer
 		end
+		
+		it 'can add many numbers' do
+			calc = Calculator.new
+			actual = calc.add(1, 2, 3, 4)
+			expect(actual).to eq 10
+		end
 	end
 
 end
